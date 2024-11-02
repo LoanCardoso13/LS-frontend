c126cf3f79818bccad6dfdd06a23345de853502f
Loan Cardoso <jow13_122@yahoo.com>
2024-11-02 10:47:42 -0400

    Functions exercises set up.

---
+++ Exercises/JS225_Object_Oriented/Functions/school_improved.js
@@ -0,0 +1,13 @@
+/*
+
+In an earlier exercise, we created a school object. It works, however, it can still be improved. The following are improvements for you to implement in the solution provided:
+
+Make the list students private. Right now, anyone can gain access to it and manipulate it.
+Make the constraint for allowed values for years a private variable. As a private variable it avoids an unnecessary statement in the addStudent method and at the same time makes the code more declarative.
+Make the getCourse function accessible in the addGrade method also. As it is, only the courseReport method has access.
+If you need a refresher on creating private data and scope, you may find the following lessons helpful:
+
+1) Creating a Private Scope with an IIFE
+2) Creating Private Data with an IIFE
+
+*/
