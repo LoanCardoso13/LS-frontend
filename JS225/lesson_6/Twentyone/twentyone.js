856bc8fff0499cba6b1070c0196f347d118a0425
Loan Cardoso <jow13_122@yahoo.com>
2024-11-14 10:13:22 -0500

    Twenty-one code spike completed.

---
+++ JS225/lesson_6/Twentyone/twentyone.js
@@ -0,0 +1,59 @@
+let readlineSync = require('readline-sync');
+
+class Hand {
+
+  isBusted() {
+
+  }
+
+  total() {
+
+  }
+}
+
+class Player {
+
+  hit() {
+
+  }
+
+  stay() {
+
+  }
+}
+
+class Dealer {
+
+  hit() {
+
+  }
+
+  stay() {
+
+  }
+}
+
+class Deck {
+
+}
+
+class Card {
+
+}
+
+class Game {
+  constructor() {
+    this.deck = new Deck();
+  }
+
+  start() {
+    // Deal cards
+    // show_initial_cards
+    // player_turn
+    // dealer_turn
+    // show_result
+  }
+}
+
+const game = new Game();
+game.start();
