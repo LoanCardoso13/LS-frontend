5b10bca8d1453dc55d69af1c182902dfc9240d7e
Loan Cardoso <jow13_122@yahoo.com>
2025-01-10 15:49:01 -0500

    JS230 programming_languages_info concluded.

--- Exercises/JS230_Frontend_Dev/GUI_Apps/programming_languages_info.js
+++ Exercises/JS230_Frontend_Dev/GUI_Apps/programming_languages_info.js
@@ -53,3 +53,60 @@ const languages = [
 ];
 
 */
+
+const languages = [
+  {
+    name: 'Ruby',
+    description: 'Ruby is a dynamic, reflective, object-oriented, ' +
+    'general-purpose programming language. It was designed and developed in the mid-1990s ' +
+    'by Yukihiro Matsumoto in Japan. According to its creator, Ruby was influenced by Perl, ' +
+    'Smalltalk, Eiffel, Ada, and Lisp. It supports multiple programming paradigms, ' +
+    'including functional, object-oriented, and imperative. It also has a dynamic type ' +
+    'system and automatic memory management.'
+  },
+
+  {
+    name: 'JavaScript',
+    description: 'JavaScript is a high-level, dynamic, untyped, and interpreted ' +
+    'programming language. It has been standardized in the ECMAScript language ' +
+    'specification. Alongside HTML and CSS, JavaScript is one of the three core ' +
+    'technologies of World Wide Web content production; the majority of websites employ ' +
+    'it, and all modern Web browsers support it without the need for plug-ins. JavaScript ' +
+    'is prototype-based with first-class functions, making it a multi-paradigm language, ' +
+    'supporting object-oriented, imperative, and functional programming styles.'
+  },
+
+  {
+    name: 'Lisp',
+    description: 'Lisp (historically, LISP) is a family of computer programming languages ' +
+    'with a long history and a distinctive, fully parenthesized prefix notation. ' +
+    'Originally specified in 1958, Lisp is the second-oldest high-level programming ' +
+    'language in widespread use today. Only Fortran is older, by one year. Lisp has changed ' +
+    'since its early days, and many dialects have existed over its history. Today, the best '+
+    'known general-purpose Lisp dialects are Common Lisp and Scheme.'
+  }
+];
+
+$(function() {
+  languages.forEach(lang => {
+    const $langContainer = $('<div>', {class: 'lang', "data-lang": lang.name});
+    $langContainer.append($('<h2>').text(lang.name));
+    $langContainer.append($('<p>').text(lang.description.split('').slice(0, 120).join('') + '...'));
+    $langContainer.append($('<a>', {class: 'more', href: '#'}).text('Show More'));
+    $('#languages').append($langContainer);
+  });
+
+  $('a.more').on('click', e => {
+    const $langContainer = $(e.target).parent();
+    const langChosen = $langContainer.attr("data-lang");
+    const text = languages.filter(lang => lang.name === langChosen)[0].description;
+    console.log(e.target.textContent)
+    if (e.target.textContent === "Show More") {
+      $langContainer.find('p').text(text);
+      $langContainer.find('a.more').text("Show Less");
+    } else if (e.target.textContent === "Show Less") {
+      $langContainer.find('p').text(text.split('').slice(0, 120).join('') + '...')
+      $langContainer.find('a.more').text("Show More");
+    }
+  });
+});
