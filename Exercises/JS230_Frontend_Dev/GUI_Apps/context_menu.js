785d36c464095e7e36cb4c7c9dfb3248b4dfe074
Loan Cardoso <jow13_122@yahoo.com>
2025-01-11 06:51:19 -0500

    JS230 both todos exercises concluded.

--- Exercises/JS230_Frontend_Dev/GUI_Apps/context_menu.js
+++ Exercises/JS230_Frontend_Dev/GUI_Apps/context_menu.js
@@ -15,3 +15,67 @@ When the user right clicks a todo item, display a context menu.
 * When the user clicks the Delete Todo option, display the confirmation dialog.
 
 */
+
+const todos = [
+  {title: "Homework", id:"1"},
+  {title: "Shopping", id:"2"},
+  {title: "Calling Mom", id:"3"},
+  {title: "Coffe with John", id:"4"},
+]
+const todosTemplate = Handlebars.compile($('#todos_template').html());
+const confirmTemplate = Handlebars.compile($('#confirm_template').html());
+const contextMenuTemplate = Handlebars.compile($('#context_menu_template').html());
+
+function confirmationListeners() {
+  $('a.confirm_yes').add('a.confirm_no').on('click', e => {
+    const choice = e.target.textContent;
+    const id = $(e.target).closest('div.confirm_wrapper').attr("data-id");
+    if (choice === "Yes") {
+      $('#todos li').filter(`[data-id=${id}]`).remove();
+    }
+    $('.overlay').css('display', 'none');
+    $('.confirm_prompt').css('display', 'none');
+  })
+}
+
+$(function() {
+  $('#todos').append(todosTemplate({todos}));
+
+  $('#todos li').on('contextmenu', e => {
+    e.preventDefault();
+    const id = $(e.target).attr("data-id");
+    $('div.context_menu').empty();
+
+    const $contextMenu = $( contextMenuTemplate({id}) );
+    $('div.context_menu').css({
+      "display": "block",
+      "position": "relative",
+    }).offset({
+      left: e.clientX,
+      top: e.clientY,
+    }).append($contextMenu);
+
+    addDelete()
+  });
+
+  function addDelete() {
+    $('li.remove').on('click', e => {
+      const promptData = {id: $(e.target).attr("data-id")};
+      $('div.confirm_prompt').empty();
+      $('div.context_menu').empty();
+      $('div.confirm_prompt').append(confirmTemplate(promptData));
+      $('.overlay').css('display', 'block');
+      $('.confirm_prompt').css('display', 'block');
+      confirmationListeners();
+    });
+  }
+
+  // $('span.remove').on('click', e => {
+  //   const promptData = {id: $(e.target).parent().attr("data-id")};
+  //   $('div.confirm_prompt').empty();
+  //   $('div.confirm_prompt').append(confirmTemplate(promptData));
+  //   $('.overlay').css('display', 'block');
+  //   $('.confirm_prompt').css('display', 'block');
+  //   confirmationListeners();
+  // });
+});
--- Exercises/JS230_Frontend_Dev/GUI_Apps/delete_confirmation.js
+++ Exercises/JS230_Frontend_Dev/GUI_Apps/delete_confirmation.js
@@ -23,3 +23,37 @@ todo_items = [
 You can use the id property for each todo object to identify the todo item to remove from the DOM. Most applications use a data-id HTML attribute to identify DOM items.
 
 */
+
+const todos = [
+  {title: "Homework", id:"1"},
+  {title: "Shopping", id:"2"},
+  {title: "Calling Mom", id:"3"},
+  {title: "Coffe with John", id:"4"},
+]
+const todosTemplate = Handlebars.compile($('#todos_template').text());
+const confirmTemplate = Handlebars.compile($('#confirm_template').text());
+
+function confirmationListeners() {
+  $('a.confirm_yes').add('a.confirm_no').on('click', e => {
+    const choice = e.target.textContent;
+    const id = $(e.target).closest('div.confirm_wrapper').attr("data-id");
+    if (choice === "Yes") {
+    $('#todos li').filter(`[data-id=${id}]`).remove();
+    }
+    $('.overlay').css('display', 'none');
+    $('.confirm_prompt').css('display', 'none');
+  })
+}
+
+$(function() {
+  $('#todos').append(todosTemplate({todos}));
+
+  $('span.remove').on('click', e => {
+    const promptData = {id: $(e.target).parent().attr("data-id")};
+    $('div.confirm_prompt').empty();
+    $('div.confirm_prompt').append(confirmTemplate(promptData));
+    $('.overlay').css('display', 'block');
+    $('.confirm_prompt').css('display', 'block');
+    confirmationListeners();
+  });
+});
