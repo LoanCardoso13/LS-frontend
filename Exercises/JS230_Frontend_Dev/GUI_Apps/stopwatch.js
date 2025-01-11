e46d13f049bb53538f91b0bc1bd0be371b65336b
Loan Cardoso <jow13_122@yahoo.com>
2025-01-11 10:24:07 -0500

    JS230 stopwatch concluded.

--- Exercises/JS230_Frontend_Dev/GUI_Apps/stopwatch.js
+++ Exercises/JS230_Frontend_Dev/GUI_Apps/stopwatch.js
@@ -44,3 +44,74 @@ When the user clicks the Reset button:
 * The timer resets all counters to 00.
 
 */
+
+const $hours = $('span.hours');
+let hoursCounter = 0;
+function increaseHour() {
+  hoursCounter++;
+  if (hoursCounter === 100) {
+    hoursCounter = 0;
+  }
+  if (hoursCounter < 10) hoursCounter = `0${hoursCounter}`;
+  $hours.text(hoursCounter);
+}
+const $mins = $('span.mins');
+let minsCounter = 0;
+function increaseMin() {
+  minsCounter++;
+  if (minsCounter === 60) {
+    increaseHour();
+    minsCounter = 0;
+  }
+  if (minsCounter < 10) minsCounter = `0${minsCounter}`;
+  $mins.text(minsCounter);
+}
+const $secs = $('span.secs');
+let secsCounter = 0;
+function increaseSec() {
+  secsCounter++;
+  if (secsCounter === 60) {
+    increaseMin();
+    secsCounter = 0;
+  }
+  if (secsCounter < 10) secsCounter = `0${secsCounter}`;
+  $secs.text(secsCounter);
+}
+const $centisecs = $('span.centisecs');
+let centisecsId;
+let centisecsCounter = 0;
+function increaseCentisec() {
+  centisecsCounter++;
+  if (centisecsCounter === 100) {
+    increaseSec();
+    centisecsCounter = 0;
+  }
+  if (centisecsCounter < 10) centisecsCounter = `0${centisecsCounter}`;
+  $centisecs.text(centisecsCounter);
+}
+
+$(function() {
+  $('button.toggle').on('click', e => {
+    if (e.target.textContent === "Start") {
+      centisecsId = setInterval(() => {
+        increaseCentisec();
+      }, 10);
+      e.target.textContent = "Stop";
+    } else if (e.target.textContent === "Stop") {
+      clearInterval(centisecsId);
+      e.target.textContent = "Start";
+    }
+  });
+  $('button.reset').on('click', e => {
+    clearInterval(centisecsId);
+    $centisecs.text('00');
+    centisecsCounter = 0;
+    $secs.text('00');
+    secsCounter = 0;
+    $mins.text('00');
+    minsCounter = 0;
+    $hours.text('00');
+    hoursCounter = 0;
+    $('button.toggle').text('Start');
+  })
+});
