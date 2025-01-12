c92df6d575be8e92b47656a0cab164a6b2535f20
Loan Cardoso <jow13_122@yahoo.com>
2025-01-12 14:55:10 -0500

    JS230 calculator concluded.

--- Exercises/JS230_Frontend_Dev/GUI_Apps/calculator.js
+++ Exercises/JS230_Frontend_Dev/GUI_Apps/calculator.js
@@ -27,3 +27,64 @@ Operations
 * When the user clicks the = button, use the current operation and the current entry to calculate the final result. Clear the current operation window and display the result in the entry window.
 
 */
+
+$currentNum = $('p.current_num');
+$calculation = $('p.calculation');
+$digit = $('#buttons a.digit');
+$op = $('#buttons a.op');
+$control = $('#buttons a.control');
+$result = $('#buttons a.result_button');
+
+let operator;
+let operand;
+let typing = false;
+const calculate = {
+  "+": (n1, n2) => n1 + n2,
+  "-": (n1, n2) => n1 - n2,
+  "x": (n1, n2) => n1 * n2,
+  "/": (n1, n2) => n1 / n2,
+};
+
+function updateScreen() {
+  let n1 = Number(operand);
+  let n2 = Number($currentNum.text());
+  $currentNum.text(calculate[operator](n1, n2));
+}
+
+$(function() {
+
+  $digit.on('click', e => {
+    if (!typing) {
+      $currentNum.empty();
+      typing = true;
+    }
+
+    const digit = e.target.textContent;
+
+    $currentNum.text($currentNum.text() + digit);
+  });
+
+  $op.on('click', e => {
+    typing = false;
+
+    if (operator) {
+      $calculation.append(`${$currentNum.text()} ${e.target.textContent} `);
+      updateScreen();
+      operand = $currentNum.text();
+      operator = e.target.textContent;
+    } else {
+      operand = $currentNum.text();
+      operator = e.target.textContent;
+      $calculation.append(`${$currentNum.text()} ${operator} `);
+    }
+  });
+
+  $result.on('click', e => {
+    typing = false;
+
+    $calculation.empty();
+    updateScreen();
+    operand = false;
+    operator = false;
+  });
+});
