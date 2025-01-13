6149daf9c4523d61dc6097626976bbcbef869867
Loan Cardoso <jow13_122@yahoo.com>
2025-01-13 18:26:19 -0500

    JS230 form_val_P1 concluded.

--- Exercises/JS230_Frontend_Dev/Forms_Validation/form_val_P2.js
+++ Exercises/JS230_Frontend_Dev/Forms_Validation/form_val_P2.js
@@ -14,3 +14,48 @@ Specifications
 * Use the keydown event to prevent the user from entering non-numeric characters in the credit card inputs.
 
 */
+
+$(function() {
+  function validateInput(inputElement) {
+    const field = $(`label[for="${inputElement.id}"]`).text();
+    
+    if (!inputElement.validity.valid) {
+      $(inputElement).addClass('invalid_field');
+      if (inputElement.validity.patternMismatch && field !== "Password") {
+        $(inputElement).parent().find('.error_message').text(`Please Enter a valid ${field}.`); 
+      } else {
+        $(inputElement).parent().find('.error_message').text(`Password must be at least 10 characters long.`); 
+      }
+      if (inputElement.validity.valueMissing) $(inputElement).parent().find('.error_message').text(`${field} is a required field.`); 
+    }
+
+    if (!$('form input').hasClass('invalid_field')) $('p.form_errors').text('');
+  }
+
+  $('form input').on('focusout', e => {
+    validateInput(e.target);
+  });
+
+  $('form input').on('focusin', e => {
+    $(e.target).removeClass('invalid_field');
+    $(e.target).parent().find('.error_message').text('');
+  });
+
+  $('form').on('submit', e => {
+    e.preventDefault();
+    const msg = "Form cannot be submitted until errors are corrected."
+
+    $('form input').toArray().forEach(validateInput)
+
+    if ($('form input').hasClass('invalid_field')) $('p.form_errors').text(msg);
+  });
+
+  $('#first_name').add('#last_name').on('keypress', e => {
+    console.log(/[a-zA-Z\s]/.test(e.key))
+    if (!/[a-zA-Z\s]/.test(e.key)) e.preventDefault();
+  });
+
+  $('#phone').add('input[name="credit_card"]').on('keydown', e => {
+    if (!/[0-9]/.test(e.key) && e.key !== "Backspace") e.preventDefault();
+  })
+});
