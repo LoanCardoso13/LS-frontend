62ba3c04a5bcb1a20050c5eece852fc7b5a7b90a
Loan Cardoso <jow13_122@yahoo.com>
2025-01-15 09:20:35 -0500

    JS230 serial_form concluded.

--- Exercises/JS230_Frontend_Dev/Forms_Validation/serial_form.js
+++ Exercises/JS230_Frontend_Dev/Forms_Validation/serial_form.js
@@ -11,3 +11,60 @@ Specifications
 * Render the form data as text in the block element you created earlier.
 
 */
+
+$(function() {
+  function validateInput(inputElement) {
+    const field = $(`label[for="${inputElement.id}"]`).text();
+    
+    if (!inputElement.validity.valid) {
+      $(inputElement).addClass('invalid_field');
+      if (inputElement.validity.patternMismatch && field !== "Password") {
+        $(inputElement).parent().find('.error_message').text(`Please Enter a valid ${field}.`); 
+      } else {
+        $(inputElement).parent().find('.error_message').text(`Password must be at least 10 characters long.`); 
+      }
+      if (inputElement.validity.valueMissing) $(inputElement).parent().find('.error_message').text(`${field} is a required field.`); 
+    }
+
+    if (!$('form input').hasClass('invalid_field')) $('p.form_errors').text('');
+  }
+
+  $('form input').on('focusout', e => {
+    validateInput(e.target);
+  });
+
+  $('form input').on('focusin', e => {
+    $(e.target).removeClass('invalid_field');
+    $(e.target).parent().find('.error_message').text('');
+  });
+
+  $('form').on('submit', e => {
+    e.preventDefault();
+    const msg = "Form cannot be submitted until errors are corrected.";
+
+    $('form input').toArray().forEach(validateInput);
+
+    if ($('form input').hasClass('invalid_field')) {
+      $('p.form_errors').text(msg);
+    } else {
+      const creditCardNumber = $('#cd1').val() + $('#cd2').val() + $('#cd3').val() + $('#cd4').val();
+      $('#credit_card').val(creditCardNumber);
+      $('#serialized').text($('form').serialize());
+    }
+  });
+
+  $('#first_name').add('#last_name').on('keypress', e => {
+    if (!/[a-zA-Z\s]/.test(e.key)) e.preventDefault();
+  });
+
+  $('#phone').add('input[name="credit_card"]').on('keydown', e => {
+    if (!/[0-9]/.test(e.key) && e.key !== "Backspace") e.preventDefault();
+  });
+
+  $('#cd1').add('#cd2').add('#cd3').add('#cd4').on('keyup', e => {
+    if (
+      e.target.value.length === 4 &&
+      $(e.target).next().text() === '-'
+    ) $(e.target).next().next().focus();
+  });
+});
