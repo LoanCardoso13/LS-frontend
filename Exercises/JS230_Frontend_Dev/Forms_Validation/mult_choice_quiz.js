4d6c50b4ce76b6d1657e79f3f5f95df4ad39d24d
Loan Cardoso <jow13_122@yahoo.com>
2025-01-15 11:06:40 -0500

    JS230 mult_choice_quiz concluded.

--- Exercises/JS230_Frontend_Dev/Forms_Validation/mult_choice_quiz.js
+++ Exercises/JS230_Frontend_Dev/Forms_Validation/mult_choice_quiz.js
@@ -42,3 +42,75 @@ const questions = [
 const answerKey = { '1': 'Douglas Adams', '2': '42', '3': 'A drink', '4': 'Betelgeuse' };
 
 */
+
+const questions = [
+  {
+    id: 1,
+    description: "Who is the author of <cite>The Hitchhiker's Guide to the Galaxy</cite>?",
+    options: ['Dan Simmons', 'Douglas Adams', 'Stephen Fry', 'Robert A. Heinlein'],
+  },
+  {
+    id: 2,
+    description: 'Which of the following numbers is the answer to Life, the \
+                  Universe and Everything?',
+    options: ['66', '13', '111', '42'],
+  },
+  {
+    id: 3,
+    description: 'What is Pan Galactic Gargle Blaster?',
+    options: ['A drink', 'A machine', 'A creature', 'None of the above'],
+  },
+  {
+    id: 4,
+    description: 'Which star system does Ford Prefect belong to?',
+    options: ['Aldebaran', 'Algol', 'Betelgeuse', 'Alpha Centauri'],
+  },
+];
+
+const answerKey = { '1': 'Douglas Adams', '2': '42', '3': 'A drink', '4': 'Betelgeuse' };
+
+const questionTemplate = Handlebars.compile($('#question_template').html());
+
+const wrongAns = ans => `Wrong answer. The correct answer is: ${ans}.`;
+const emptyAns = ans => `You didn't answer this question. The correct answer is: ${ans}.`;
+const rightAns = 'Correct answer';
+
+$(function() {
+  questions.forEach(question => $('form fieldset').append(questionTemplate(question)));
+
+  $('a.submit').on('click', e => {
+    e.preventDefault();
+
+    if (!$('a.submit').hasClass('disabled')) {
+      $('div.question').toArray().forEach(question => {
+        const $question = $(question);
+
+        if ($question.find('input:checked').length === 0) {
+          $question.find('p.result').addClass('wrong');
+          $question.find('p.result').text(emptyAns(answerKey[$question.attr("data-id")]));
+        } else {
+
+          if ( $question.find('input:checked').val() !== answerKey[$question.attr("data-id")] ) {
+            $question.find('p.result').addClass('wrong');
+            $question.find('p.result').text(wrongAns(answerKey[$question.attr("data-id")]));
+          } else {
+            $question.find('p.result').addClass('correct');
+            $question.find('p.result').text(rightAns);
+          }
+
+        }
+      });
+      $('a.submit').addClass('disabled');
+      $('a.reset_form').removeClass('disabled');
+    }
+  });
+
+  $('a.reset_form').on('click', e => {
+    e.preventDefault();
+
+    $('form')[0].reset();
+    $('a.submit').removeClass('disabled');
+    $('a.reset_form').addClass('disabled');
+    $('p.result').removeClass('wrong correct').text('');
+  })
+});
